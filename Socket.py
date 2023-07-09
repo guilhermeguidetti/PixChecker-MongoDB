6de4271defcdb1f51cfcb9c046bd908171a2c90a
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2023-07-08 23:25:36 -0300

    Removed feat: check_single_instance

--- Socket.py
+++ Socket.py
@@ -16,27 +16,10 @@ def check_internet_connection():
     except OSError:
         return False
 
-def check_single_instance():
-    current_pid = os.getpid()
-    for process in psutil.process_iter(['name', 'cmdline']):
-        try:
-            process_name = process.info['name']
-            process_cmdline = process.info['cmdline']
-            if "pixchecker" in process_name and "python" in process_cmdline:
-                pid = process.pid
-                if pid != current_pid:
-                    # Outra instância do aplicativo já está em execução, exibe uma mensagem de erro e sai
-                    messagebox.showerror("Erro", "O aplicativo Pix Checker já está em execução.")
-                    exit()
-        except (psutil.Error, psutil.AccessDenied):
-            continue
-
 storeName = getStoreName()
 
 # Verifica a conexão com a Internet antes de abrir o aplicativo
-if check_internet_connection():
-    # Verifica se já existe uma instância do aplicativo em execução
-    check_single_instance()
+if check_internet_connection(): 
 
     # Conexão estabelecida e nenhum bloqueio encontrado, inicie o aplicativo
     app = PixCheckerApp(storeName)
